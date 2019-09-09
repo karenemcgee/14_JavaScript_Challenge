c64c8b5446a7b5f0119cfc932ca9238b7f6fe9a0
Karen McGee <karen.eliz.mcgee@gmail.com>
2019-09-09 18:05:26 -0500

    append table to webpage successful

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,1 +1,16 @@
 console.log(data);
+
+var tbody = d3.select("tbody");
+
+// Append table to webpage
+data.forEach(function(ufoData) {
+    console.log(ufoData);
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  
