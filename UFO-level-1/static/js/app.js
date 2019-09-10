10ce7f5b91918699f35a6591f82003ac94bce1ad
Karen McGee <karen.eliz.mcgee@gmail.com>
2019-09-09 21:19:25 -0500

    can filter and print console but not on page yet

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,16 +1,36 @@
-console.log(data);
+// console.log(data);
+
+var ufoData = data;
 
 var tbody = d3.select("tbody");
 
 // Append table to webpage
-data.forEach(function(ufoData) {
-    console.log(ufoData);
+data.forEach(function(ufoTable) {
+    //console.log(ufoTable);
     var row = tbody.append("tr");
-    Object.entries(ufoData).forEach(function([key, value]) {
-      console.log(key, value);
+    Object.entries(ufoTable).forEach(function([key, value]) {
+      //console.log(key, value);
       var cell = row.append("td");
       cell.text(value);
     });
   });
 
-  
+// Find date/time rows matching date input
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(ufoData);
+
+    var filteredData = ufoData.filter(event => event.datetime === inputValue);
+
+    console.log(filteredData);
+});
+
+// i'm getting the right data in the console
+// how do i get this data to appear?
+// how do i get everything to reset/pull different dates if a user wants to filter another way?
