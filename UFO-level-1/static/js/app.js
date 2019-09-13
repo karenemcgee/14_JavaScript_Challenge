da99b251a78e788a54782bfef56734ca3c64248e
Karen McGee <karen.eliz.mcgee@gmail.com>
2019-09-13 13:08:34 -0500

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,18 +4,23 @@ var ufoData = data;
 
 var tbody = d3.select("tbody");
 
-// Append table to webpage
-data.forEach(function(ufoTable) {
-    //console.log(ufoTable);
-    var row = tbody.append("tr");
-    Object.entries(ufoTable).forEach(function([key, value]) {
-      //console.log(key, value);
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
 
-// Find date/time rows matching date input
+// Append table to webpage
+function createTable(data) {
+  tbody.html("");
+
+  data.forEach(function(ufoTable) {
+      //console.log(ufoTable);
+      var row = tbody.append("tr");
+      Object.entries(ufoTable).forEach(function([key, value]) {
+        //console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  }
+
+// Filter table by user input 
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
@@ -24,13 +29,13 @@ button.on("click", function() {
     var inputValue = inputElement.property("value");
 
     console.log(inputValue);
-    console.log(ufoData);
 
-    var filteredData = ufoData.filter(event => event.datetime === inputValue);
+    var filteredData = data.filter(event => event.datetime === inputValue);
 
     console.log(filteredData);
-});
+    
+    createTable(filteredData);
+  });
 
-// i'm getting the right data in the console
-// how do i get this data to appear?
-// how do i get everything to reset/pull different dates if a user wants to filter another way?
+createTable(ufoData);
+
